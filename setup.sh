ffde3209d2a644a1a51a53fea1cae96eea955143
Marc Capell <marc.capell@gmail.com>
2019-02-24 16:42:32 +0100

    Changes after fresh install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Install ansible...
+if ! [ -x "$(command -v ansible-playbook)" ]; then
+  echo "Ansible is not installed. Installing it..." >&2
+  apt install ansible
+fi
 
 # Run ansible playbook
 ansible-playbook -i "localhost," -c local ubuntu_setup.yml
