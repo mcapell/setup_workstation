f785793bff16922c3eca0caf84076ba436fde90d
Marc Capell <marc.capell@gmail.com>
2021-09-06 23:46:56 +0200

    Setup fish and neovim as development environment

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Install ansible...
 if ! [ -x "$(command -v ansible-playbook)" ]; then
   echo "Ansible is not installed. Installing it..." >&2
-  apt install ansible
+  sudo apt install ansible
 fi
 
 # Run ansible playbook
-ansible-playbook -i "localhost," -c local ubuntu_setup.yml
+ansible-playbook ubuntu_setup.yml
